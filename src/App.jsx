9c9e61c277a1706deeb1a2f07d20d081ecd84017
Omar Faruk <omar15-3851@diu.edu.bd>
2023-09-12 02:19:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
